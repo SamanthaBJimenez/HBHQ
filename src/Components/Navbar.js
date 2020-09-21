8f277cdaaad909806600776f76b48867c0519cdd
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-09-20 21:14:27 -0400

    create navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link, Element, animateScroll as scroller } from 'react-scroll';
 
 const Navbar = () => {
     return (
-        <nav class="nav nav-fill fixed-top">
-            <a class="nav-link" href="#about">About</a>
-            <a class="nav-link" href="#podcast">Podcast</a>
-            <a class="nav-link" href="#youtube">Youtube</a>
-            <a class="nav-link" href="#contact">Contact</a>
+        <nav class="nav fixed-top navbar navbar-expand-lg">
+            <ul class="navbar-nav">
+                <li class="nav-link"><Link to='about'  activeClass='active-nav' spy={true} smooth={true} duration={500}>About</Link></li>
+                <li class="nav-link"><Link to='podcast'  activeClass='active-nav' spy={true} smooth={true} duration={500}>Podcast</Link></li>
+                <li class="nav-link"><Link to='youtube'  activeClass='active-nav' spy={true} smooth={true} duration={500}>Youtube</Link></li>
+                <li class="nav-link"><Link to='contact'  activeClass='active-nav' spy={true} smooth={true} duration={500}>Contact</Link></li>
+            </ul>
         </nav>
     )
 }
