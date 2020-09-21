6fcafbf05cc2bb7b17b72b0b60bdaf86b18cd206
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-09-21 13:40:29 -0400

    update about component

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,8 +3,11 @@ import React from 'react';
 const About = () => {
     return(
         <div class='about-main'>
-            <h1> About </h1>
-            <h2>We are a cannabis lifestyle brand with videos and podcasts coming out weekly to educate and entertain the masses.</h2>
+            <h1 class='brand-name'>Highbred HQ</h1>
+            {/* <div class='about-card'> */}
+                <h1> About </h1>
+                <h4 style={{"font-weight": "200"}}>We are a cannabis lifestyle brand with videos and podcasts coming out weekly to educate and entertain the masses.</h4>
+            {/* </div> */}
         </div>
     )
 }
